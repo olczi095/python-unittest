f0f005391695b7685110d542d436bf6129f4fe85
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-11-21 11:40:23 +0100

    Add chapter eight

--- 08_employee/tests/test_emp_2.py
+++ 08_employee/tests/test_emp_2.py
@@ -4,25 +4,28 @@ from employee.emp import Employee
 
 class TestEmployee(unittest.TestCase):
 
+    def setUp(self):
+        print('setting up...')
+        self.emp = Employee('John', 'Smith', 80000)
+
+    def tearDown(self):
+        print('tearing down...')
+        del self.emp
+
     def test_email(self):
-        emp = Employee('John', 'Smith', 80000)
-        self.assertEqual(emp.email, 'john.smith@mail.com')
+        self.assertEqual(self.emp.email, 'john.smith@mail.com')
 
     def test_email_after_changing_first_name(self):
-        emp = Employee('John', 'Smith', 80000)
-        emp.first_name = 'Adam'
-        self.assertEqual(emp.email, 'adam.smith@mail.com')
+        self.emp.first_name = 'Adam'
+        self.assertEqual(self.emp.email, 'adam.smith@mail.com')
 
     def test_email_after_changing_last_name(self):
-        emp = Employee('John', 'Smith', 80000)
-        emp.last_name = 'Kowalski'
-        self.assertEqual(emp.email, 'john.kowalski@mail.com')
+        self.emp.last_name = 'Kowalski'
+        self.assertEqual(self.emp.email, 'john.kowalski@mail.com')
 
     def test_tax(self):
-        emp = Employee('John', 'Smith', 80000)
-        self.assertEqual(emp.tax, 13600)
+        self.assertEqual(self.emp.tax, 13600)
 
     def test_salary_after_applying_bonus(self):
-        emp = Employee('John', 'Smith', 80000)
-        emp.apply_bonus()
-        self.assertEqual(emp.salary, 88000)
+        self.emp.apply_bonus()
+        self.assertEqual(self.emp.salary, 88000)
