f2c1e5218f2b1758e0f0ff7d1f3cad38d9fd5372
Aleksandra Dąbrowska <aleksandradabrowska095@gmail.com>
2022-11-21 11:42:34 +0100

    Add chapter twelve

---
+++ 12_mocking/01_basic/02_patch.py
@@ -0,0 +1,17 @@
+import random
+from unittest.mock import patch
+
+
+def get_value():
+    return random.randint(0, 9)
+
+@patch('random.randint')
+def test_get_value(mock_random):
+    mock_random.return_value = 3
+    result = get_value()
+    result = get_value()
+    print(result)
+    assert result == 3
+    assert mock_random.call_count == 1
+
+test_get_value()
